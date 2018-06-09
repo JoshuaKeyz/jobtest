4824adf32e79e5350a3dd7fb0545f262ec6c82e0
JoshuaKeyz <joshua.oguma@outlook.com>
2018-06-09 22:49:05 +0300

    Added some tests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// *** register as a contractor ***//
+router.get("/register_contractor", (req, res,next)=>{});
+router.post("/register_contractor", (req, res,next)=>{});
+
+router.get("/register_consumer", (req, res,next)=>{})
+router.post("/register_consumer", (req, res,next)=>{});
+
+router.get("/provide_location", (req, res,next)=>{});
+router.put("/provide_location", (req, res,next)=>{});
+
+router.get("revise_quote", (req, res,next)=>{})
+router.patch("revise_quote", (req, res,next)=>{});
+
 
 module.exports = router;
